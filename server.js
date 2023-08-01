a1ff8af0d7c1e833961f29929c8cba997c2aee53
DeveshPatel-14 <devpatel1130@gmail.com>
2023-08-01 20:21:24 +0530

    removed manual cors

--- server.js
+++ server.js
@@ -13,14 +13,6 @@ connectDB()
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 app.use(express.json());
 
 const root = path.join(__dirname,'build')
