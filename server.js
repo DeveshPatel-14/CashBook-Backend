b9b5d4b1b894fc4c3b001f88d74a29290aad9215
DeveshPatel-14 <devpatel1130@gmail.com>
2023-08-01 17:11:43 +0530

    updated cors option

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ import path from 'path'
 dotenv.config()
 connectDB()
 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+
 const app = express()
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 
 
