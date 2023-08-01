aeff36433c28d98dea8685f81ea132d71e02da93
DeveshPatel-14 <devpatel1130@gmail.com>
2023-08-01 20:29:43 +0530

    updated cors

--- server.js
+++ server.js
@@ -14,19 +14,27 @@ connectDB()
 const app = express();
 
 app.use(express.json());
-
+app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 const root = path.join(__dirname,'build')
 app.use(express.static(root))
 
-app.get('*', (req, res) => {
-  res.sendFile('index.html', { root })
-})
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve('build', 'index.html'))
+);
+
 
 // Available routes
 app.use('/api/users', userRoutes)
 app.use('/api/transactions', transactionRoutes)
 
-app.get('/', (req, res) => 
+app.get('/test', (req, res) => 
 {
     res.send('API is running....')
 })
